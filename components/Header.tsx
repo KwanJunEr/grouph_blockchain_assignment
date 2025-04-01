715cc2a1d6dd877a5862b87049ffa7ad7d66637e
KwanJunEr <jonastheitguy@gmail.com>
2025-04-01 16:10:20 +0800

    finish setting up integration with metakmask

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,69 @@
-"use client"
+"use client";
 
-import { useState, useEffect } from "react"
-import Image from 'next/image'
-import React from 'react'
-import { Button } from './ui/button'
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { ConnectButton } from "thirdweb/react";
+import { client } from "@/lib/client";
+import { generatePayload, isLoggedIn, login, logout } from "@/actions/login";
+import { sepolia } from "thirdweb/chains";
 
 const Header = () => {
+  const router = useRouter();
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  // Ensure login state is checked on component mount
+  useEffect(() => {
+    const checkLogin = async () => {
+      const userLoggedIn = await isLoggedIn();
+      setLoggedIn(userLoggedIn);
+    };
+    checkLogin();
+  }, []);
+
   return (
-    <header className="absolute top-0 left-0 w-full px-5 py-6 bg-transparent z-50">
+    <header
+      className={`${
+        loggedIn ? "sticky headerImage" : "absolute bg-transparent"
+      } top-0 left-0 w-full px-5 py-6 z-50 transition-all duration-300`}
+    >
       <div className="flex justify-between items-center">
-        <div className='flex flex-row space-x-5'>
-           <Image src="/Union.png" width={15} height={15} alt="logo" />
-           <h2 className='text-xl text-white font-extrabold'>HealthSync</h2>
+        <div className="flex flex-row space-x-5">
+          <Image src="/Union.png" width={15} height={15} alt="logo" />
+          <h2 className="text-xl text-white font-extrabold">HealthSync</h2>
         </div>
-       
-        <Button className='bg-white text-black w-[200px] hover:bg-gray-400/45 ease-in-out transition-all duration-300'>Connect Wallet</Button>
+        <div>
+          <ConnectButton
+            connectButton={{
+              label: "Sign in",
+            }}
+            chain={sepolia}
+            client={client}
+            auth={{
+              isLoggedIn: async (address) => {
+                console.log("checking if logged in!", { address });
+                return await isLoggedIn();
+              },
+              doLogin: async (params) => {
+                console.log("logging in!");
+                await login(params);
+                setLoggedIn(true); // Update state
+                router.push("/dashboard");
+              },
+              getLoginPayload: async ({ address }) =>
+                generatePayload({ address }),
+              doLogout: async () => {
+                console.log("logging out!");
+                await logout();
+                setLoggedIn(false);
+                router.push("/");
+              },
+            }}
+          />
+        </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
