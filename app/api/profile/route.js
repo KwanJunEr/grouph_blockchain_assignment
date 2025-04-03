f2e3c37c76e73aba91f34e290b49ef1ef29fb118
KwanJunEr <jonastheitguy@gmail.com>
2025-04-03 16:08:00 +0800

    fixed on EditProfileModal

--- app/api/profile/route.js
+++ app/api/profile/route.js
@@ -2,26 +2,16 @@ import { NextResponse } from "next/server";
 import { createHash } from 'crypto';
 import PatientData from "@/models/patientGeneralInfo"
 
-const generateHashID = (data) => {
-  const hash = createHash('sha256');  // Use SHA-256 for hashing (or any other hashing algorithm)
-  const dataString = JSON.stringify(data);  // Convert the data to a string format
-
-  hash.update(dataString);  // Update the hash with the data
-
-  return hash.digest('hex');  // Return the hash as a hexadecimal string
-};
 
 export async function POST(req) {
   try {
   
   // Get form data from the request body
-    const { name, age, gender, bloodType, houseAddress, height, weight, email, phone, chronicConditions, allergies, medications, userAddress } = await req.json();
-    console.log("Received data:", { name, age, gender, bloodType, houseAddress, height, weight, email, phone, chronicConditions, allergies, medications, userAddress });
+    const { documentHash, userAddress, name, age, gender, bloodType, houseAddress, height, weight, email, phone, chronicConditions, allergies, medications } = await req.json();
+    console.log("Received data:", { name, age, gender, bloodType, houseAddress, height, weight, email, phone, chronicConditions, allergies, medications, userAddress, documentHash});
     // Validate required fields
 
     // Generate a unique document hash
-    const documentHash = generateHashID({ userAddress, name });
-
     const newUserProfile = {
       name,
       age,
